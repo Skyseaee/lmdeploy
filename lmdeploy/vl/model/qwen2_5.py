96393795ea93389a7e51c405004f3051176da574
wenlong.cao <wenlong.cao@shopee.com>
2025-07-25 18:21:58 +0800

    refactor Qwen2.5VL

--- lmdeploy/vl/model/qwen2_5.py
+++ lmdeploy/vl/model/qwen2_5.py
@@ -6,12 +6,12 @@ Date: 2025-02-24 17:00:12
 """
 import sys
 import os
-from typing import Dict, List
+from typing import Dict, List, Tuple
 
+import numpy as np
 import torch
 import torch.nn.functional as F
 from transformers import AutoConfig
-from PIL.Image import Image
 
 from lmdeploy.vl.model.base import VISION_MODELS, VisonModel
 from lmdeploy.vl.model.utils import disable_logging
@@ -27,6 +27,7 @@ def check_qwen_2d5_vl_deps_install():
         import qwen_vl_utils  # noqa: F401
     except ImportError:
         os.system(f"{sys.executable} -m pip install qwen_vl_utils")
+        os.execv(sys.executable, [sys.executable] + sys.argv)
         try:
             import qwen_vl_utils  # noqa: F401
         except ImportError:
@@ -309,54 +310,51 @@ class Qwen2d5VLModel(VisonModel):
 
         images = self.collect_images(messages)
         optional_keys = {'resized_height', 'resized_width', 'min_pixels', 'max_pixels'}
-        outputs = []
+        content = []
         for image, params in images:
             image = image.convert('RGB')
             item = dict(type='image', image=image)
             item.update({key: params[key] for key in params.keys() if key in optional_keys})
-            image_inputs, _ = process_vision_info([dict(content=[item])])
-            result = self.processor.image_processor(images=image_inputs, videos=None, return_tensors='pt')
-            merge_length = self.processor.image_processor.merge_size**2
-            image_tokens = result['image_grid_thw'].prod(dim=1) // merge_length
-            result.update(dict(image_size=image.size, image_tokens=image_tokens, image_token_id=self.image_token_id))
-
-        messages.append(dict(role='preprocess', content=outputs))
+            content.append(item)
+        
+        image_inputs, _ = process_vision_info([dict(content=content)])
+        result = self.processor.image_processor(images=image_inputs, videos=None, return_tensors='pt')
+        merge_length = self.processor.image_processor.merge_size**2
+        image_tokens = result['image_grid_thw'].prod(dim=1) // merge_length
+        result.update(dict(image_size=image.size, image_tokens=image_tokens, image_token_id=self.image_token_id))
+        messages.append(dict(role='preprocess', content=result))
         return messages
 
     @torch.no_grad()
     def forward(self, messages: List[Dict], max_batch_size: int = 1) -> List[Dict]:
         image_inputs = [x['content'] for x in messages if x['role'] == 'preprocess'][0]
-        # if VLM_ENABLE_TRT:
-        #     new_hidden_states, rotary_pos_emb, attention_mask, cu_attention_mask, window_index = \
-        #         self.model.visual.prepare_input(pixel_values, image_grid_thw, self.hf_config._attn_implementation)
-        #     input_data = {
-        #         "pixel_values": new_hidden_states.cuda(),
-        #         "rotary_pos_emb": rotary_pos_emb.cuda(),
-        #         "attention_mask": attention_mask.cuda(),
-        #         "cu_attention_mask": cu_attention_mask.cuda(),
-        #     }
-        #     hidden_states = self.model.vision_model_trt(input_data)
-        #     reverse_indices = torch.argsort(window_index)
-        #     image_embeds = hidden_states[reverse_indices, :]
-        #     image_embeds = image_embeds.to(dtype=torch.float)
-        # else:
         outputs = []
-        for image in image_inputs:
-            pixel_values = image["pixel_values"]
-            image_grid_thw = image["image_grid_thw"]
-            image_tokens = image["image_tokens"]
-
+        pixel_values = image_inputs["pixel_values"]
+        image_grid_thw = image_inputs["image_grid_thw"]
+        image_tokens = image_inputs["image_tokens"]
+        if VLM_ENABLE_TRT:
+            new_hidden_states, rotary_pos_emb, attention_mask, cu_attention_mask, window_index = \
+                self.model.visual.prepare_input(pixel_values, image_grid_thw, self.hf_config._attn_implementation)
+            input_data = {
+                "pixel_values": new_hidden_states.cuda(),
+                "rotary_pos_emb": rotary_pos_emb.cuda(),
+                "attention_mask": attention_mask.cuda(),
+                "cu_attention_mask": cu_attention_mask.cuda(),
+            }
+            hidden_states = self.model.vision_model_trt(input_data)
+            reverse_indices = torch.argsort(window_index)
+            image_embeds = hidden_states[reverse_indices, :]
+            image_embeds = image_embeds.to(dtype=torch.float)
+        else:
             image_embeds = self.model.visual(pixel_values, grid_thw=image_grid_thw).to(dtype=torch.float)
             logger.info(f"vision_encoder: pixel_values: {list(pixel_values.shape)}, image_embeds:{list(image_embeds.shape)}")
-            # image_embeds = image_embeds.split(image_tokens.tolist())
-            # for i, embeddings in enumerate(image_embeds):
-            #     outputs.append(
-            #         dict(embeddings=embeddings,
-            #             grid_thw=image_inputs['image_grid_thw'][i].tolist()))
-            image.update({"embeddings":image_embeds})
-            outputs.append(image)
+            image_embeds = image_embeds.split(image_tokens.tolist())
+        for i, embeddings in enumerate(image_embeds):
+            outputs.append(
+                dict(embeddings=embeddings,
+                    grid_thw=image_inputs['image_grid_thw'][i].tolist()))
         messages.append(dict(role='forward', content=outputs))
-        return outputs
+        return messages
 
     @staticmethod
     def proc_messages(messages, chat_template, sequence_start):
@@ -386,10 +384,100 @@ class Qwen2d5VLModel(VisonModel):
         prompt = chat_template.messages2prompt(prompt_messages, sequence_start)
         return prompt, IMAGE_TOKEN
 
+    def get_mrope_info(self,
+                       seq_len: int,
+                       grid_thws: List[Tuple[int, int, int]] = None,
+                       embedding_ranges: List[Tuple[int, int]] = None):
+        if grid_thws is None:
+            mrope_position_ids = torch.arange(seq_len).expand(3, -1)
+            mrope_position_delta = torch.tensor([0], dtype=torch.long)
+        else:
+            mrope_position_ids = [
+                torch.arange(embedding_ranges[0][0]).expand(3, -1)
+            ]
+            st_idx = embedding_ranges[0][0]
+            for i, (grid_thw, embedding_range) in enumerate(
+                    zip(grid_thws, embedding_ranges)):
+                llm_grid_t, llm_grid_h, llm_grid_w = grid_thw
+                llm_grid_h //= 2
+                llm_grid_w //= 2
+                t_index = torch.arange(llm_grid_t).view(-1, 1).expand(
+                    -1, llm_grid_h * llm_grid_w).flatten()
+                h_index = torch.arange(llm_grid_h).view(1, -1, 1).expand(
+                    llm_grid_t, -1, llm_grid_w).flatten()
+                w_index = torch.arange(llm_grid_w).view(1, 1, -1).expand(
+                    llm_grid_t, llm_grid_h, -1).flatten()
+                mrope_position_ids.append(
+                    torch.stack([t_index, h_index, w_index]) + st_idx)
+                st_idx += max(llm_grid_h, llm_grid_w)
+                if i < len(embedding_ranges) - 1:
+                    text_len = embedding_ranges[i +
+                                                1][0] - embedding_ranges[i][1]
+                else:
+                    text_len = seq_len - embedding_range[1]
+                mrope_position_ids.append(
+                    torch.arange(text_len).expand(3, -1) + st_idx)
+                st_idx += text_len
+            mrope_position_ids = torch.cat(mrope_position_ids, dim=-1)
+            mrope_position_delta = torch.tensor([st_idx - seq_len],
+                                                dtype=torch.long)
+
+        return mrope_position_ids, mrope_position_delta
+    
+    def to_turbomind_aux(self, messages, prompt, IMAGE_TOKEN, tokenizer, sequence_start):
+        """Auxiliary function to pack the forwarding results in a format
+        compatible with what is required by turbomind engine.
+
+        Args:
+            messages(List[Dict]): the output of `preprocess`
+            prompt(str): the prompt after applying chat template
+            IMAGE_TOKEN(str): a placeholder where image tokens will be
+                inserted
+            tokenzer: the tokenizer model
+            sequence_start: starting flag of a sequence
+        """
+        # collect image features from messages
+        features = [x['content'] for x in messages if x['role'] == 'forward'][0]
+        grid_thws = [x['grid_thw'] for x in features]
+        features = [x['embeddings'] for x in features]
+        features = [x.cpu().numpy() for x in features]
+        # split prompt into segments and validate data
+        segs = prompt.split(IMAGE_TOKEN)
+        assert len(segs) == len(features) + 1, (f'the number of {IMAGE_TOKEN} is not equal '
+                                                f'to input images, {len(segs) - 1} vs {len(features)}')
+
+        # tokenizer prompt, and get input_embeddings and input_embedding_ranges
+        input_ids = []
+        begins = []
+        ends = []
+        for i, seg in enumerate(segs):
+            if i > 0 and i <= len(features):
+                image_dim = features[i - 1].shape[0]
+                begins.append(len(input_ids))
+                ends.append(begins[-1] + image_dim)
+                input_ids.extend([self.image_token_id] * image_dim)
+            seg_ids = tokenizer.encode(seg, add_bos=((i == 0) and sequence_start))
+            input_ids.extend(seg_ids)
+        ranges = np.stack([begins, ends], axis=1).tolist()
+        # Qwen2.5VL MRope Position IDs
+        mrope_position_ids, mrope_position_delta = self.get_mrope_info(
+                                seq_len=len(input_ids), grid_thws=grid_thws, embedding_ranges=ranges)
+        return dict(prompt=prompt, 
+                    input_ids=input_ids, 
+                    input_embeddings=features, 
+                    input_embedding_ranges=ranges,
+                    mrope_position_ids=mrope_position_ids,
+                    mrope_position_delta=mrope_position_delta)
+
     def to_turbomind(self, messages, chat_template, tokenizer, sequence_start):
         prompt, IMAGE_TOKEN = self.proc_messages(messages, chat_template, sequence_start)
         return self.to_turbomind_aux(messages, prompt, IMAGE_TOKEN, tokenizer, sequence_start)
     
+    def to_pytorch(self, messages, chat_template, tokenizer, sequence_start):
+        """Return to the information needed by pytorch engine."""
+        prompt, IMAGE_TOKEN = self.proc_messages(messages, chat_template, sequence_start)
+        return self.to_pytorch_aux(messages, prompt, IMAGE_TOKEN, tokenizer, sequence_start)
+
 def qwen2d5vl_ut():
     # import sys
     # from lmdeploy.vl.model.onepiece.workflow import build_engine
